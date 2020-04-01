810622d49a4e01939fb4900575acfb251fa46df1
Slava Kovalev <reqwiem96@mail.ru>
2020-04-01 19:01:13 +0300

    Вывел функцию с проверкой является и передаваемое значение строкой и если больше 30 символов меняет оставшиеся на многоточие

--- script/script.js
+++ script/script.js
@@ -1,37 +1,19 @@
 'use strict'
+let randomName = prompt('Напишите что-нибудь');
 
-//Переменные
-let lang = prompt('Введите ru иди en'),
-namePerson = prompt('Ввеите имя'),
-daysArray = [{ru : 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье',
-en: 'Monday, thuesday, wensday, thursday, friday, saturday, sunday'}];
+function chooseName(prompt){
+    prompt = prompt.trim();
+    if (prompt !== String(prompt)){
+        console.log('Данное значение не явяется строкой');
+    }else if(prompt.length > 30){
+        prompt = prompt.slice(0,30)
+        prompt += '...'
+        console.log(prompt);
+    }else{
+        console.log(prompt);
+    }
+    
+    return;
+};
 
-
-//if else и выводы в консоль
-if (lang == 'ru'){
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-}else if (lang == 'en'){
-    console.log('Monday, thuesday, wensday, thursday, friday, saturday, sunday');
-}else{
-    console.log('Я же сказал пиши ru или en незачем отсебятину придумывать =Ъ');
-}
-
-//switch
-
-switch(lang){
-    case 'ru':
-        console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-        break;
-    case 'en':
-        console.log('Monday, thuesday, wensday, thursday, friday, saturday, sunday');
-        break;
-    default:
-        console.log("Неверно");
-}
-
-//Вывод в консоль
-console.log(daysArray[0][lang]);
-
-//Тернарные операторы
-let result = namePerson == 'Артем' ? 'Директор' : namePerson == 'Максим' ? 'Преподаватель' : 'Студент';
-console.log(result);
+chooseName(randomName);
