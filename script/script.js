78937db4f8205e6a8d9d6b7e712e54c34d95f447
Kombrero <62717158+Kombrero@users.noreply.github.com>
2020-04-07 00:36:29 +0500

    Update script.js
    Доделал

--- script/script.js
+++ script/script.js
@@ -1,21 +1,3 @@
-// let day;
-
-// function getWeekDay(date){
-//     let date = new Date();
-//     let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', '<i>'+'Суббота'+'</i>', '<i>'+'Воскресенье'+'</i>'];
-//     day = date.getDate();
-
-//     return week[day];
-// }
-// let date = new Date();
-// getWeekDay(date);
-// console.log('getWeekDay(date);: ', getWeekDay(date));
-
-// console.log(day);
-// for (i = 0; i < week.length; i++) {
-    
-//     document.write(week[i] + '<br>' );
-// }
 let week = ['<i>' + 'Воскресенье' + '</i>', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', '<i>' + 'Суббота' + '</i>'];
 
 function getWeekDay(date) {
